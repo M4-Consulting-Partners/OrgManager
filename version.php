a2d043f33773c80cb6d28406a3cc777b2a64dfd1
Dirk <Dirk.Maschke@m4-cp.de>
2018-04-12 11:55:00 +0200

    Create version.php

---
+++ version.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ ***********************************************************************************************
+ * Versionsdatei fuer das Admidio-Plugin Organisations Chart
+ *
+ * @copyright 2017-2017 M4 Consulting Partners
+ * @see http://www.m4-cp.de/
+ * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0 only
+ *
+ ***********************************************************************************************
+ */
+
+$plugin_version = '0.1.0';
+$plugin_stand 	= '13.01.2017';
